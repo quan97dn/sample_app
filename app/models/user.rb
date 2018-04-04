d291ed3760890a13c09b5c861106e71a2d90eea8
Nguyen Hoang Quan <quan105dn@gmail.com>
2018-04-04 11:44:55 +0700

    Chapter 6

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: {maximum: Settings.user.name_length}
+  before_save{email.downcase!}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: Settings.user.email_length},
+            format: {with: VALID_EMAIL_REGEX},
+            uniqueness: {case_sensitive: false}
+  has_secure_password
+  validates :password, presence: true, length: {minimum: Settings.user.password_length}
+end
