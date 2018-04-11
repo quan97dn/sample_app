ffab657f197a9a0ec424e36e84ed3e45b9b86f5f
Nguyen Hoang Quan <quan105dn@gmail.com>
2018-04-11 08:37:38 +0700

    Chapter_8

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,6 +7,9 @@ module ActiveSupport
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     include ApplicationHelper
     fixtures :all
+    def is_logged_in?
+      !session[:user_id].nil?
+    end
   end
   # Add more helper methods to be used by all tests here...
 end
